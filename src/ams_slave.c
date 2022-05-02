c81085e706ebb9b2e2fa08a21334f27debac38ef
tvaugr <thomas.vanacker@ugentracing.be>
2022-05-02 14:06:14 +0200

    added register map to setup function

--- src/ams_slave.c
+++ src/ams_slave.c
@@ -84,7 +84,53 @@ char ams_slave_test_spi(ams_slave* s){
 /* Setup all registers of the slave IC with the correct values */
 void ams_slave_setup(ams_slave* s){
 	/* Checkout "slave_registers.txt" for more info */
-	/* TODO */
+	ams_slave_write(s, 0x01, 0b00000010);
+	ams_slave_write(s, 0x02, 0b10000000);
+	ams_slave_write(s, 0x03, 0b10000000);
+	ams_slave_write(s, 0x04, 0b11111100); /* CHECK! */
+	ams_slave_write(s, 0x05, 0b01111111); /* CHECK! */
+	ams_slave_write(s, 0x06, 0b11111111); /* TODO */
+	ams_slave_write(s, 0x07, 0b00000000); /* TODO */
+	ams_slave_write(s, 0x08, 0b11111111); /* TODO */
+	ams_slave_write(s, 0x09, 0b00000000);
+	ams_slave_write(s, 0x0a, 0b00001111); /* TODO */
+	ams_slave_write(s, 0x0b, 0b11111111); /* TODO */
+	ams_slave_write(s, 0x0c, 0b00000000);
+	ams_slave_write(s, 0x0d, 0b00000000);
+	ams_slave_write(s, 0x0e, 0b10110100); /* TODO */
+	ams_slave_write(s, 0x0f, 0b11111111); /* TODO */
+	ams_slave_write(s, 0x10, 0b00000000); /* TODO */
+	ams_slave_write(s, 0x11, 0b00000000);
+	ams_slave_write(s, 0x12, 0b00010000);
+	ams_slave_write(s, 0x13, 0b00000000); /* TODO */
+	ams_slave_write(s, 0x14, 0b00000000); /* TODO */
+	ams_slave_write(s, 0x15, 0b00000000); /* TODO */
+	ams_slave_write(s, 0x16, 0b00000000); /* TODO */
+	ams_slave_write(s, 0x17, 0b00000000); /* TODO */
+	ams_slave_write(s, 0x18, 0b00000000); /* TODO */
+	ams_slave_write(s, 0x19, 0b00000000); /* TODO */
+	ams_slave_write(s, 0x1a, 0b00000000); /* TODO */
+	ams_slave_write(s, 0x1b, 0b11000000); /* TODO */
+	ams_slave_write(s, 0x1c, 0b00000000); /* TODO */
+	ams_slave_write(s, 0x1d, 0b00000000); /* TODO */
+	ams_slave_write(s, 0x1e, 0b00000000); /* TODO */
+	ams_slave_write(s, 0x1f, 0b00000000); /* TODO */
+	ams_slave_write(s, 0x20, 0b00000000); /* TODO */
+	ams_slave_write(s, 0x21, 0b00000000); /* TODO */
+	ams_slave_write(s, 0x22, 0b00000000); /* TODO */
+	ams_slave_write(s, 0x23, 0b00000000); /* TODO */
+	ams_slave_write(s, 0x24, 0b00000000); /* TODO */
+	ams_slave_write(s, 0x25, 0b00000000); /* TODO */
+	ams_slave_write(s, 0x26, 0b00000000); /* TODO */
+	ams_slave_write(s, 0x27, 0b00000000); /* TODO */
+	ams_slave_write(s, 0x28, 0b00000000); /* TODO */
+	ams_slave_write(s, 0x29, 0b00000000); /* TODO */
+	ams_slave_write(s, 0x2a, 0b00000000); /* TODO */
+	ams_slave_write(s, 0x2b, 0b00000000); /* TODO */
+	ams_slave_write(s, 0x2c, 0b00000000); /* TODO */
+	ams_slave_write(s, 0x2d, 0b00000000); /* TODO */
+	ams_slave_write(s, 0x2e, 0b00000000); /* TODO */
+	ams_slave_write(s, 0x2f, 0b00000000); /* TODO */
 }
 
 
